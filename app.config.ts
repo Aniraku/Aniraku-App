7ffa896adb730467bb32816afc97e11c87cf77a0
Shoislam0311 <sho.islam0311@gmail.com>
2026-09-14 21:40:15 +0600

    fix: add react-native-reanimated plugin for Android build

--- app.config.ts
+++ app.config.ts
@@ -47,6 +47,7 @@ const config: ExpoConfig = {
     "expo-audio",
     "expo-font",
     "expo-web-browser",
+    "react-native-reanimated",
     ["react-native-video", { enableAndroidPictureInPicture: true }],
     ["expo-screen-orientation", { initialOrientation: "DEFAULT" }],
     ["expo-secure-store", { configureAndroidBackup: false }],
